5813c312fee1f327dbfd140663dc03116740a696
Patrick <pfriis@outlook.com>
2025-09-16 10:04:19 +0200

    Added simple math methods

--- CalculatorClassLibrary/Calculator.cs
+++ CalculatorClassLibrary/Calculator.cs
@@ -2,5 +2,23 @@
 
 public class Calculator
 {
-    
+    public int Add(int x, int y)
+    { 
+        return x + y; 
+    }
+
+    public int Subtract(int x, int y) 
+    { 
+        return x - y; 
+    }
+
+    public int Multupli(int x, int y)
+    {  
+        return x * y;
+    }
+
+    public int Divide(int x, int y)
+    {  
+        return x / y; 
+    }
 }
