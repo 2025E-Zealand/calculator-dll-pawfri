56211d44afad553b4a9a8450cb8c343154c34f7b
Patrick <pfriis@outlook.com>
2025-09-16 18:15:17 +0200

    Added Equal Unit Test for Add

---
+++ CalculatorClassLibraryTests/CalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClassLibrary.Tests;
+
+[TestClass()]
+public class CalculatorTests
+{
+    [DataRow(0, 10)]
+    [DataRow(1, 9)]
+    [DataRow(5, 5)]
+    [DataRow(2, 8)]
+    [TestMethod]
+    public void AddTest(int value, int value2)
+    {
+        // Arrange
+        var calculator = new Calculator();
+
+        // Act
+        int result = calculator.Add(value, value2);
+
+        // Assert
+        Assert.AreEqual(result, 10);
+    }
+
+    //[TestMethod]
+    //public void SubtractTest()
+    //{
+    //    // Arrange
+
+
+    //    // Act
+
+
+    //    // Assert
+    //    Assert.Fail();
+    //}
+
+    //[TestMethod]
+    //public void MultiplyTest()
+    //{
+    //    // Arrange
+
+    //    // Act
+
+    //    // Assert
+    //    Assert.Fail();
+    //}
+
+    //[TestMethod]
+    //public void DivideTest()
+    //{
+    //    // Arrange
+
+    //    // Act
+
+    //    // Assert
+    //    Assert.Fail();
+    //}
+}
