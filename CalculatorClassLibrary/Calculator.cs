4e87cfd14d5238e4b28fe8bb42871bd0f626f73a
Patrick <pfriis@outlook.com>
2025-09-16 10:07:47 +0200

    Added summaries and corrected typo

--- CalculatorClassLibrary/Calculator.cs
+++ CalculatorClassLibrary/Calculator.cs
@@ -2,21 +2,45 @@
 
 public class Calculator
 {
+    /// <summary>
+    /// Add two numbers
+    /// </summary>
+    /// <param name="x">First number</param>
+    /// <param name="y">Second number</param>
+    /// <returns></returns>
     public int Add(int x, int y)
     { 
         return x + y; 
     }
 
+    /// <summary>
+    /// Subtract two numbers
+    /// </summary>
+    /// <param name="x">First number</param>
+    /// <param name="y">Second number</param>
+    /// <returns></returns>
     public int Subtract(int x, int y) 
     { 
         return x - y; 
     }
 
-    public int Multupli(int x, int y)
+    /// <summary>
+    /// Multiply two numbers
+    /// </summary>
+    /// <param name="x">First number</param>
+    /// <param name="y">Second number</param>
+    /// <returns></returns>
+    public int Multiply(int x, int y)
     {  
         return x * y;
     }
 
+    /// <summary>
+    /// Divide two numbers
+    /// </summary>
+    /// <param name="x">First number</param>
+    /// <param name="y">Second number</param>
+    /// <returns></returns>
     public int Divide(int x, int y)
     {  
         return x / y; 
